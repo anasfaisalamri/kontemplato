6e17bb03da5c65172acce4a710033a8240091d45
anasfaisalamri <anasfaisalamri@gmail.com>
2023-03-14 19:26:29 +0700

    add authenticate controller & view

---
+++ application/views/auth/register.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title><?= $title; ?></title>
+  <link rel="shortcut icon" href="<?= base_url('assets/'); ?>img/icon/logo.png" type="image/x-icon">
+
+  <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/bootstrap.min.css">
+  <!-- Custom fonts for this template-->
+  <link href="<?= base_url('assets/'); ?>fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+  <div class="container">
+
+    <div class="card o-hidden border-0 shadow-lg my-5 col-lg-6 mx-auto">
+      <div class="card-body p-0">
+        <!-- Nested Row within Card Body -->
+        <div class="row">
+          <div class="col-lg">
+            <div class="p-5">
+              <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">Form Registration</h1>
+                <img class="mb-3" src="<?= base_url('assets/img/icon/logo.png') ?>" width="200">
+              </div>
+              <form class="user" action="<?= base_url('auth/register'); ?>" method="post">
+                <div class="form-group">
+                  <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name" value="<?= set_value('name'); ?>">
+                  <?= form_error('name', '<small class="text-danger ps-2">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Username" value="<?= set_value('username'); ?>">
+                  <?= form_error('username', '<small class="text-danger ps-2">', '</small>'); ?>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email'); ?>">
+                  <?= form_error('email', '<small class="text-danger ps-2">', '</small>'); ?>
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-6 mb-3 mb-sm-0">
+                    <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                    <?= form_error('password1', '<small class="text-danger ps-2">', '</small>'); ?>
+                  </div>
+                  <div class="col-sm-6">
+                    <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
+                    <?= form_error('password2', '<small class="text-danger ps-2">', '</small>'); ?>
+                  </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-user btn-block">
+                  Register Account
+                </button>
+              </form>
+              <hr>
+              <div class="text-center">
+                <a class="small" href="<?= base_url('auth'); ?>">Already have an account? Login!</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="<?= base_url('assets/'); ?>js/jquery.min.js"></script>
+  <script src="<?= base_url('assets/'); ?>js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?= base_url('assets/'); ?>js/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
---
+++ application/views/home/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="<?= base_url('assets/'); ?>css/bootstrap.min.css" rel="stylesheet">
+
+  <title><?= $title; ?></title>
+  <link rel="shortcut icon" href="<?= base_url('assets/'); ?>img/icon/logo.png" type="image/x-icon">
+</head>
+
+<body>
+  <h1>Hello, world!</h1>
+  <script src="<?= base_url('assets/'); ?>js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
