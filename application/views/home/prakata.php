386e667e3693caa6621dc99a7a751b3d45f709af
anasfaisalamri <anasfaisalamri@gmail.com>
2023-10-26 14:25:30 +0700

    tambah halaman about/prakata

---
+++ application/views/home/prakata.php
@@ -0,0 +1,10 @@
+<?php include_once 'partials/navbars.php' ?>
+
+<div class="container my-5 py-5">
+  <h1 class="font-serif text-center mb-4 display-5">Prakata</h1>
+  <div class="row justify-content-center">
+    <div class="col-md-10 mt-4">
+      <p class="mb-5">Kontemplato merupakan media alternatif yang sengaja dibentuk untuk mengisi ruang kajian dan literasi yang inklusif di Indonesia. Kontemplato merepresentasikan isi kajian yang penuh dengan kontemplasi-renungan dengan kebulatan pikiran atau perhatian penuh. Kata "Plato" terinspirasi dari nama seorang tokoh filsuf asal Yunani yang terkenal dengan pemikirannya yang penuh dengan perenungan di dunia filsafat. Kontemplato akan menyebarkan informasi mulai dari isu-isu politik, budaya populer, dan kehidupan sehari-hari dengan menggunakan bahasa yang inklusif melalui sudut pandang kritis agar mampu melahirkan suatu karya yang dapat memantik para pembaca, khususnya Milenial yang sudah familiar dengan budaya populer. Kontemplato menyakini bahwa literasi yang inklusif dan menyertakan bahasa yang mudah dipahami-penjabaran isi kajian yang mendalam, tetapi memosisikan pembaca sebagai awam - contohnya seperti budaya populer yang sudah banyak dikenal oleh masyarakat luas, secara tidak langsung dapat berpengaruh terhadap meningkatnya budaya membaca.</p>
+    </div>
+  </div>
+</div>
