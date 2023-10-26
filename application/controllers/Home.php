5fcbb3f0fbbcd8203f8088358164cd20bde44f7f
anasfaisalamri <anasfaisalamri@gmail.com>
2023-10-26 14:54:56 +0700

    update method untuk homepage

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -31,14 +31,17 @@ class Home extends CI_Controller
     $data['categories'] = $this->kontemplato->getCategories();
     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
-    $data['title'] = 'Kontemplato | About';
+    $data['title'] = 'Kontemplato';
     $this->load->view('home/layouts/header', $data);
-    $this->load->view('home/about', $data);
+    $this->load->view('home/prakata', $data);
     $this->load->view('home/layouts/footer', $data);
   }
 
-  public function show($slug)
+  public function show($slug = null)
   {
+    if (empty($slug)) {
+      redirect('home/notfound');
+    }
     $data['categories'] = $this->kontemplato->getCategories();
     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
@@ -50,5 +53,55 @@ class Home extends CI_Controller
     $this->load->view('home/layouts/header', $data);
     $this->load->view('home/show', $data);
     $this->load->view('home/layouts/footer', $data);
+  }
+
+  public function posts()
+  {
+    $data['categories'] = $this->kontemplato->getCategories();
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['posts'] = $this->kontemplato->getAllPosts(null, null);
+
+    $data['title'] = 'All Posts';
+    $data['desc'] = 'Seluruh tulisan dari penulis dan dari seluruh kategori';
+
+    $keyword = $this->input->post('keyword');
+
+    if ($this->input->post('keyword')) {
+      $data['posts'] = $this->kontemplato->getAllPosts(null, null, $keyword);
+    }
+
+    $category = $this->db->get_where('categories', ['slug' => $this->input->get('category')])->row_array();
+    $data['category'] = $this->input->get('category');
+
+    if ($this->input->get('category') && $this->input->get('category') !== $category['slug']) {
+      redirect('home/notfound');
+      die;
+    } else if ($this->input->get('category')) {
+      $category = $this->db->get_where('categories', ['slug' => $this->input->get('category')])->row_array();
+      $data['title'] = $category['category'];
+      $data['desc'] = $category['desc'];
+      $data['posts'] = $this->kontemplato->getPostByCategory($category['id']);
+    }
+
+    $this->load->view('home/layouts/header', $data);
+    $this->load->view('home/posts', $data);
+    $this->load->view('home/layouts/footer', $data);
+  }
+
+  public function categories()
+  {
+    $data['categories'] = $this->kontemplato->getCategories();
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+    $data['title'] = 'Kontemplato | Kategori';
+    $this->load->view('home/layouts/header', $data);
+    $this->load->view('home/categories', $data);
+    $this->load->view('home/layouts/footer', $data);
+  }
+
+  public function notFound()
+  {
+    $data['title'] = '404 Not Found';
+    $this->load->view('home/notfound');
   }
 }
