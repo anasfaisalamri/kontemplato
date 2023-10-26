81948e1a49954288dd497b71e89e3bcabedbee0e
anasfaisalamri <anasfaisalamri@gmail.com>
2023-10-26 14:27:29 +0700

    perubahan ui di bagian footer

--- application/views/home/layouts/footer.php
+++ application/views/home/layouts/footer.php
@@ -2,19 +2,24 @@
   <div class="container">
     <div class="d-none d-sm-block">
       <div class="row justify-content-between pt-5">
-        <div class="d-flex justify-content-between text-white text-capitalize mb-5">
+        <div class="d-flex justify-content-between text-white text-capitalize mb-3">
           <div class="col-md-6">
             <div class="scroll-to-top d-flex align-items-center">
               <img src="<?= base_url('assets'); ?>/img/icon/logo-white.png" alt="logo-white.png" width="50">
               <h1 class="text-uppercase fw-bold ms-1 mb-0">Kontemplato</h1>
             </div>
+            <ul class="list-unstyled d-flex m-0 fs-1 mt-3">
+              <li class="ms-1"><a class="text-secondary nav-link link-light" href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+              <li class="ms-5"><a class="text-secondary nav-link link-light" href="https://instagram.com/kontemplato.media" target="_blank"><i class="fab fa-instagram"></i></a></li>
+              <li class="ms-5"><a class="text-secondary nav-link link-light" href="#" target="_blank"><i class="fas fa-envelope"></i></a></li>
+            </ul>
           </div>
 
           <div class="col-6 col-md-2">
             <h5>Section</h5>
             <ul class="nav flex-column">
               <li class="nav-item"><a href="<?= base_url(); ?>" class="nav-link link-light p-0 text-secondary">Home</a></li>
-              <li class="nav-item"><a href="<?= base_url('home/about'); ?>" class="nav-link link-light p-0 text-secondary">About</a></li>
+              <li class="nav-item"><a href="<?= base_url('home/about'); ?>" class="nav-link link-light p-0 text-secondary">Prakata</a></li>
               <?php if ($user) { ?>
                 <li class="nav-item dropdown link-light text-secondary">
                   <a class="nav-link dropdown-toggle link-light text-secondary p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -37,26 +42,19 @@
           </div>
 
           <div class="col-6 col-md-2">
-            <h5>Kategori</h5>
+            <h5><a href="<?= base_url('home/categories'); ?>" class="nav-link link-light p-0 text-white">Kategori</a></h5>
             <ul class="nav flex-column">
-              <li class="nav-item">
-                <a href="<?= base_url('home/ornamen') ?>" class="nav-link link-light text-secondary p-0">Ornamen</a>
-              </li>
-              <li class="nav-item">
-                <a href="<?= base_url('home/kontemplato'); ?>" class="nav-link link-light text-secondary p-0">Kontemplato</a>
-              </li>
+              <?php foreach ($categories as $category) { ?>
+                <li class="nav-item">
+                  <a href="<?= base_url('home/posts?category=') . $category['slug']; ?>" class="nav-link link-light text-secondary p-0"><?= $category['category']; ?></a>
+                </li>
+              <?php } ?>
             </ul>
           </div>
         </div>
 
         <div class="d-flex flex-wrap text-white justify-content-between pt-3 mt-5 border-top">
           <div>Copyright &copy; Kontemplato <?= date('Y'); ?> All rights reserved.</div>
-
-          <ul class="list-unstyled d-flex m-0 fs-4">
-            <li class="ms-3"><a class="link-light" href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-            <li class="ms-3"><a class="link-light" href="https://instagram.com/kontemplato.media" target="_blank"><i class="fab fa-instagram"></i></a></li>
-            <li class="ms-3"><a class="link-light" href="#" target="_blank"><i class="fas fa-envelope"></i></a></li>
-          </ul>
         </div>
       </div>
     </div>
@@ -66,13 +64,20 @@
         <img src="<?= base_url('assets/'); ?>img/icon/logo-white.png" alt="logo-white.png" width="30">
         <h3 class="text-uppercase fw-bold ms-1 mb-0">Kontemplato</h3>
       </div>
+      <div class="d-flex align-items-center justify-content-center mt-3 pb-3 border-bottom">
+        <ul class="nav list-unstyled fs-2">
+          <li><a class="text-white" href="#"><i class="fab fa-twitter"></i></a></li>
+          <li class="ms-3"><a class="text-white" href="https://instagram.com/kontemplato.media" target="_blank"><i class="fab fa-instagram"></i></a></li>
+          <li class="ms-3"><a class="text-white" href="#"><i class="fas fa-envelope"></i></a></li>
+        </ul>
+      </div>
 
       <div class="row justify-content-center px-5 mt-4">
-        <div class="col mb-4">
+        <div class="col mb-4 section">
           <h5>Section</h5>
           <ul class="nav flex-column">
-            <li class="nav-item"><a href="<?= base_url(); ?>" class="nav-link p-0 text-secondary">Home</a></li>
-            <li class="nav-item"><a href="<?= base_url('home/about'); ?>" class="nav-link p-0 text-secondary">About</a></li>
+            <li class="nav-item"><a href="<?= base_url(); ?>" class="nav-link link-light p-0 text-secondary">Home</a></li>
+            <li class="nav-item"><a href="<?= base_url('home/about'); ?>" class="nav-link link-light p-0 text-secondary">Prakata</a></li>
             <?php if ($user) { ?>
               <li class="nav-item dropdown link-light text-secondary">
                 <a class="nav-link dropdown-toggle link-light text-secondary p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -93,32 +98,21 @@
             <?php } ?>
           </ul>
         </div>
-        <div class="col">
-          <h5>Kategori</h5>
+        <div class="col section">
+          <h5><a href="<?= base_url('home/categories'); ?>" class="nav-link link-light p-0 text-white">Kategori</a></h5>
           <ul class="nav flex-column">
-            <li class="nav-item">
-              <a href="<?= base_url('home/kontemplato'); ?>" class="nav-link link-light p-0 text-secondary">Kontemplato</a>
-            </li>
-            <li class="nav-item">
-              <a href="<?= base_url('home/ornamen'); ?>" class="nav-link link-light p-0 text-secondary">Ornamen</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="row justify-content-center mt-5 border-top">
-        <div class="col-auto mt-3">
-          <ul class="nav list-unstyled fs-2">
-            <li><a class="text-secondary" href="#"><i class="fab fa-twitter"></i></a></li>
-            <li class="ms-3"><a class="text-secondary" href="https://instagram.com/kontemplato.media" target="_blank"><i class="fab fa-instagram"></i></a></li>
-            <li class="ms-3"><a class="text-secondary" href="#"><i class="fas fa-envelope"></i></a></li>
+            <?php foreach ($categories as $category) { ?>
+              <li class="nav-item">
+                <a href="<?= base_url('home/posts?category=') . $category['slug']; ?>" class="nav-link link-light text-secondary p-0"><?= $category['category']; ?></a>
+              </li>
+            <?php } ?>
           </ul>
         </div>
       </div>
 
       <div class="row justify-content-center">
-        <div class="col-auto">
-          <p class="mt-1 mb-0 text-secondary">Copyright &copy; Kontemplato <?= date('Y'); ?></p>
+        <div class="col-11 text-center">
+          <p class="mt-1 mb-0 text-secondary copy-right">Copyright &copy; Kontemplato <?= date('Y'); ?></p>
         </div>
       </div>
     </div>
